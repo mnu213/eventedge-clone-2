e130b7913c116902d46d5eab97098e6097d0112c
Mohammed Uddin <mnu213@nyu.edu>
2022-01-15 22:54:45 -0500

    login and registration routing, sliding menu

--- front-end/src/components/app/App.jsx
+++ front-end/src/components/app/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from '../navigation/Navigation';
+import Login from '../login/Login';
+import Registration from '../registration/Registration';
+import Home from '../home/home';
 
 function App() {
   return (
     <div>
-      <Navigation />
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<Registration />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
