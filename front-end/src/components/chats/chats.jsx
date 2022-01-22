13e6e0b56f8bf1bf38e5c2bc2d2967b70a6a1035
Mohammed Uddin <mnu213@nyu.edu>
2022-01-22 10:39:01 -0500

    Add create chat modal and remove labels from all inputs in app

--- front-end/src/components/chats/chats.jsx
+++ front-end/src/components/chats/chats.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chat from './chat/Chat';
+import CreateChat from './create-chat-modal';
 
 export default function Chats() {
+  const [menuVisible, setMenuVisible] = useState(false);
   return (
     <>
-      <input />
-      <button type="button"> + </button>
+      <input type="text" placeholder="Search your chats" />
+      <button type="button" onClick={() => setMenuVisible(true)}>
+        {' '}
+        +{' '}
+      </button>
       <Chat />
       <Chat />
       <Chat />
+      {menuVisible && <CreateChat onClose={() => setMenuVisible(false)} />}
     </>
   );
 }
---
+++ front-end/src/components/chats/create-chat-modal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../modal.css';
+
+function CreateChat(props) {
+  const { onClose } = props;
+  return (
+    <div className="modal">
+      <button type="submit" onClick={onClose}>
+        X
+      </button>
+      <br />
+      <button type="submit"> Create Chat </button>
+      <br />
+      <input type="text" placeholder="Group Chat Name" />
+      <br />
+      <input type="text" placeholder="Search for users..." />
+    </div>
+  );
+}
+
+export default CreateChat;
