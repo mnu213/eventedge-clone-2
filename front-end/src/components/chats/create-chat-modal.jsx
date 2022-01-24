86ebbf8a5c48a552049fc128d4136c712bd5caa5
Mohammed Uddin <mnu213@nyu.edu>
2022-01-24 17:27:22 -0500

    adding PendingEvent components to pending events modal

--- front-end/src/components/chats/create-chat-modal.jsx
+++ front-end/src/components/chats/create-chat-modal.jsx
@@ -3,17 +3,23 @@ import styles from '../modal.css';
 
 function CreateChat(props) {
   const { onClose } = props;
+  const submit = () => {
+    alert('created event(nothing done in background)');
+  };
+
   return (
     <div className="modal">
       <button type="submit" onClick={onClose}>
         X
       </button>
       <br />
-      <button type="submit"> Create Chat </button>
-      <br />
-      <input type="text" placeholder="Group Chat Name" />
-      <br />
-      <input type="text" placeholder="Search for users..." />
+      <form onSubmit={submit}>
+        <button type="submit"> Create Chat </button>
+        <br />
+        <input type="text" placeholder="Group Chat Name" />
+        <br />
+        <input type="text" placeholder="Search for users..." />
+      </form>
     </div>
   );
 }
--- front-end/src/components/pending-events-modal/pending-events-modal.jsx
+++ front-end/src/components/pending-events-modal/pending-events-modal.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from '../modal.css';
+import PendingEvent from './pending-event/pending-event';
 
 function PendingEvents(props) {
   const { onClose } = props;
+  const pendingEvents = [];
+  const chatIds = [1, 2, 3, 4, 5];
+  const listItems = chatIds.map((chatId) => (
+    <PendingEvent key={chatId.toString()} chatId={chatId} />
+  ));
   return (
     <div className="modal">
       <button type="submit" onClick={onClose}>
         X
       </button>
+      {listItems}
     </div>
   );
 }
