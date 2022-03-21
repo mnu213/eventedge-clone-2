0510c6642eb51b05817114bde37efaf9b12ecd56
Mohammed Uddin <mnu213@nyu.edu>
2022-03-20 23:53:16 -0400

    login and register routes with database

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -1,28 +1,67 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import User from '../models/user.js';
 
-const login = (req, res) => {
-  console.log('INSIDE LOGIN ROUTE');
-  res.send('LOGIN ROUTE');
+const login = async (req, res, next) => {
+  // User.find({email: req.body.email})
+  //   .exec()
+  //   .then((users) => {
+  //     if (users.length < 1) {
+  //       res.status(401).json({message: 'Auth failed'});
+  //     } else {
+  //       res.status(222);
+  //     }
+  //   })
+  //   .catch();
+  const users = await User.find({email: req.body.email}).exec();
+  console.log(users);
+  if (users.length < 1) {
+    res.status(401).json({message: 'Auth failed'});
+  }else{
+    const match = await bcrypt.compare(req.body.password, users[0].password);
+    if (match) {
+      res.status(200).json({message: 'Login Successful'});
+    } else {
+      res.status(401).json({message: 'Auth failed'});
+    }
+  }
+
+
 };
 
 const register = (req, res, next) => {
-  const user = new User({
-    _id: mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: req.body.password,
-  });
-  user
-    .save()
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error);
+  User.find({email: req.body.email})
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        res.status(409).json({message: 'User exists'});
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            next(err);
+          } else {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: 'User created',
+                  createdUser: user,
+                });
+              })
+              .catch((error) => {
+                console.log(error);
+                next(error);
+              });
+          }
+        });
+      }
     });
-
-  console.log('INSIDE REGISTER ROUTE');
-  res.json('REGIESTER ROUTE');
 };
 
 export {login, register};
--- back-end/models/user.js
+++ back-end/models/user.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  email: String,
-  password: String,
+  email: {type: String, required: true},
+  password: {type: String, required: true},
 });
 
 export default mongoose.model('User', userSchema);
